3b1edd8849ffd0dbcc6768e80a5d0085cb6a7a68
JoseCarlos2025 <josecarlosllitra@gmail.com>
2024-02-14 22:53:33 +0000

    ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ import NotificationTest from './pages/teacher-pages/notification-test-pages/Noti
 import UserProfilePage from './pages/profile/profile';
 import Studenthome from './pages/student-pages/studenthome/studenthome';
 import StudentExercises from './pages/student-pages/studentexercises/studentexercises';
-// import AdminCourse from './pages/admincourse/admincourse';
+import AdminCourse from './pages/admincourse/admincourse';
 
 function App() {
 
@@ -56,10 +56,8 @@ function App() {
           <Route path="users" element={<UserAdmin />}></Route>
           <Route path="schools" element={<SchoolsAdmin />} />
           <Route path="school" element={<AdminSchool />} />
-
           <Route path="groups" element={<GroupAdmin />} />
           <Route path="group" element={<AdminCourse />} />
-
           <Route path="courses" element={<CoursesAdmin />} />
           <Route path="students" element={<StudentSchools />} />
           <Route path="teachers" element={<TeacherSchools />} />
