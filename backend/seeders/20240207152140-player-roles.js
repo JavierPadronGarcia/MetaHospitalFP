ee155f5116e0000dd135136407b52d66e1a0cfc7
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2024-04-09 10:28:21 +0200

    finish problems with submit grades

--- backend/seeders/20240207152140-player-roles.js
+++ backend/seeders/20240207152140-player-roles.js
@@ -5,7 +5,7 @@ const { addPlayerRoles } = require('../utils/seederUtils');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const playerRoles = addPlayerRoles(['Auxiliar', 'Enfermero', 'Médico']);
+    const playerRoles = addPlayerRoles(['Assistant', 'Nurse', 'Medic']);
     await queryInterface.bulkInsert('PlayerRoles', playerRoles);
   },
 
