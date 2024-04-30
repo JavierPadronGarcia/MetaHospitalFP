42337b967060d0de98829f4a658e2fd449a88e92
Javier <padrongarciajavier04@gmail.com>
2024-04-30 16:36:44 +0100

    add translation sync and translations files

--- frontend/server/downloadTranslations.js
+++ frontend/server/downloadTranslations.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const XLSX = require('xlsx');
 
 function downloadTranslations() {
-  const fileURL = 'https://docs.google.com/spreadsheets/d/1ZgjDaLHXk1BUxS0OfvEHqP5ZGK_fnWcEbg2_-Xadmt0/edit?pli=1#gid=1985820596';
+  const fileURL = process.env.REACT_APP_TRANSLATIONS_SHEET;
 
   axios({
     method: 'GET',
@@ -12,26 +13,53 @@ function downloadTranslations() {
     responseType: 'arraybuffer'
   })
     .then(response => {
-      const filePath = path.join(__dirname, '../public/translations.xlsx');
       const buffer = Buffer.from(response.data, 'binary');
-      fs.writeFileSync(filePath, buffer);
-      console.log('Translations file saved successfully.');
+
+      const workbook = XLSX.read(buffer, { type: 'buffer' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const translationsByLanguage = {};
+      const range = XLSX.utils.decode_range(sheet['!ref']);
+      const languages = ['en', 'es'];
+      const columnNames = ['rowIndex', 'Tname', ...languages];
+
+      range.s.r = 2;
+
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: columnNames, range })
+        .filter(row => row.Tname !== undefined);
+
+      languages.forEach(language => {
+        translationsByLanguage[language] = jsonData.map(entry => ({
+          [entry.Tname]: entry[language]
+        }));
+      });
+
+      languages.forEach(language => {
+        const languageFolderPath = path.join(__dirname, `../src/translations/${language}`);
+        if (!fs.existsSync(languageFolderPath)) {
+          fs.mkdirSync(languageFolderPath, { recursive: true });
+        }
+        const jsonFilePath = path.join(languageFolderPath, 'global.json');
+
+        const translationsObject = arrayToObject(translationsByLanguage[language]);
+
+        fs.writeFileSync(jsonFilePath, JSON.stringify(translationsObject, null, 2));
+        console.log(`Translations JSON file for ${language} saved successfully.`);
+      });
     })
     .catch(error => {
       console.error('Error downloading the translations', error);
     });
 }
 
-module.exports = downloadTranslations;
-// const workbook = XLSX.readFile(filePath);
-// const sheetName = workbook.SheetNames[0];
-// console.log(workbook.SheetNames);
-// const sheet = workbook.Sheets[sheetName];
-// const columnNames = ['rowIndex', 'Tname', 'en', 'es'];
+function arrayToObject(array) {
+  const result = {};
+  array.forEach(obj => {
+    const key = Object.keys(obj)[0];
+    const value = obj[key];
+    result[key] = value;
+  });
+  return result;
+}
 
-// const jsonData = XLSX.utils.sheet_to_json(sheet, { header: columnNames });
-
-// cron.schedule('0 0 * * *', () => {
-//    console.log('Running translation update...');
-//    downloadTranslations();
-// });
+downloadTranslations();
