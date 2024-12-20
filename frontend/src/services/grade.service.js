22f280647b59a1b23688e5ff74c8a9b9f4468728
JavierPadron <j.padron@scottandirwin.com>
2024-12-20 12:32:13 +0000

    possible fix of representacion of submitted date of grades

--- frontend/src/services/grade.service.js
+++ frontend/src/services/grade.service.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import dayjs from "dayjs";
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import i18next from "i18next";
 import { backendGradesEndpoint } from "../constants/backendEndpoints";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function getOptions(token) {
   let bearerAccess = 'Bearer ' + token;
@@ -15,13 +21,12 @@ function getOptions(token) {
   return options;
 }
 
-
 async function getGradesByExercises(exerciseId) {
   try {
     const response = await axios.get(`${backendGradesEndpoint}/findGradesByStudentInExercise/${exerciseId}`,
       getOptions(localStorage.getItem('token'))
     );
-    return response.data;
+    return parseDate(response).data;
   } catch (err) {
     throw err;
   }
@@ -32,7 +37,7 @@ async function getAllGradesOnAGroup(groupId) {
     const response = await axios.get(`${backendGradesEndpoint}/findAllGradesOfTheGroup/${groupId}`,
       getOptions(localStorage.getItem('token'))
     );
-    return response.data;
+    return parseDate(response).data;
   } catch (err) {
     throw err;
   }
@@ -43,10 +48,29 @@ async function getAllGradesOnAGroupForExcel(groupId) {
     const response = await axios.get(`${backendGradesEndpoint}/findAllGradesInGroupForExcel/${groupId}`,
       getOptions(localStorage.getItem('token'))
     );
+
+    if (response.data.length !== 0) {
+      const userTimeZone = dayjs.tz.guess();
+      response.data.forEach(exercise => {
+        exercise["Submit Date"] = dayjs(exercise["Submit Date"] + "T" + exercise["Submit Time"] + "Z").tz(userTimeZone).format("YYYY-MM-DD");
+        exercise["Submit Time"] = dayjs(exercise["Submit Date"] + "T" + exercise["Submit Time"] + "Z").tz(userTimeZone).format("HH:mm:ss");
+      })
+    }
+
     return response.data;
   } catch (err) {
     throw err;
   }
+}
+
+function parseDate(response) {
+  if (response.data.length !== 0) {
+    const userTimeZone = dayjs.tz.guess();
+    response.data.forEach(participation => {
+      participation.submittedAt = dayjs(participation.submittedAt).tz(userTimeZone).format("YYYY-MM-DD HH:mm:ss");
+    })
+  }
+  return response;
 }
 
 export default {
